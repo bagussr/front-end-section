bd75f0aa8a648ebd5b3be7b6e2ff62ae108ee176
bagussr <bagusssyamsu@gmail.com>
2022-12-08 09:50:52 +0700

    edit about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,20 +13,43 @@ const About = () => {
 
         {/* text */}
         <div className='flex flex-col gap-3 flex-1  '>
-          <h1 className='lg:text-4xl text-2xl font-bold text-white  '>
+          <h1 className='lg:text-4xl text-2xl font-bold text-white text-center '>
             Tentang Kami
           </h1>
-          <p className='text-base leading-7 tracking-wide text-justify'>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa
-            mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien
-            fringilla, mattis ligula consectetur, ultrices.
-          </p>
+          <div className='flex flex-col items-center text-center gap-5 mt-5'>
+            <span>
+              <h3 className='text-xl font-bold'>Tema</h3>
+              <p>Become a Reliable Front End Web Developer in Era 5.0</p>
+            </span>
+            <span>
+              <h3 className='text-xl font-bold'>Judul</h3>
+              <p>Software Engineering Creation Volume 3</p>
+            </span>
+            <span>
+              <h3 className='text-xl font-bold'>Tujuan</h3>
+              <ol style={{ listStyleType: 'number' }}>
+                <li>
+                  Meningkatkan pengetahuan mengenai Front End Web Developer.
+                </li>
+                <li>
+                  Meningkatkan pengetahuan mengenai pandangan dan jangkauan
+                  Front End Web Developer di masa depan.
+                </li>
+                <li>
+                  Menumbuhkan dan mengembangkan minat menjadi seorang Front End
+                  Web Developer.
+                </li>
+              </ol>
+            </span>
+          </div>
 
           {/* CTA */}
-          <div className='flex w-full md:flex-row flex-col gap-3 my-5'>
-            <button className='bg-gradient-to-r from-[#5334B1] to-[#401881] px-5 py-3 text-white rounded-lg drop-shadow-[0_1px_10px_#ffffff3f] hover:drop-shadow-[0_35px_35px_#ffffff3f] transition duration-500'>
+          <div className='flex w-full md:flex-row flex-col gap-3 my-5 justify-center'>
+            <a
+              href='https://wa.me/6289514582709'
+              className='bg-gradient-to-r from-[#5334B1] to-[#401881] px-5 py-3 text-white rounded-lg drop-shadow-[0_1px_10px_#ffffff3f] hover:drop-shadow-[0_35px_35px_#ffffff3f] transition duration-500'>
               Kontak Kami
-            </button>
+            </a>
             {/* <button className=" border border-[#5334B1] shadow-lg px-5 py-3 hover:drop-shadow-[0_35px_35px_#ffffff3f] rounded-lg transition duration-500">
               FAQ
             </button> */}
