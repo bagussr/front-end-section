af2c8535c5aab9080583bcb8f050fcc3905b7196
bagussr <bagusssyamsu@gmail.com>
2022-12-14 20:38:53 +0700

    count down & dark mode

--- src/components/countdown/CountdownTimer.jsx
+++ src/components/countdown/CountdownTimer.jsx
@@ -2,7 +2,7 @@ import useCountDown from '../../hooks/useCountDown';
 import ShowCounter from './ShowCounter';
 
 const CountdownTimer = () => {
-  const D_DAY = new Date('2022-12-10T23:59:59');
+  const D_DAY = new Date('2023-01-01T23:59:59');
   const [days, hours, minutes, seconds] = useCountDown(D_DAY);
   return (
     <ShowCounter
